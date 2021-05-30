d7a789561a8c4475877a5f25bca3122f062b2ac9
Keith Vargas <keith.vargas@outlook.com>
2021-05-30 14:46:36 +0800

    Add fix for windows

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CoE 151 MP1 Implementation
-Secret lang natin to ha
+Important notes on implementation:
+- This is not supported on Windows unfortunately due to select() taking nothing but sockets. I request this be tested on Unix machines.
+
+RFC Protocol:
+- The protocol our class follows can be seen [here](https://github.com/digitalsirkeith/coe151/blob/master/protocol/README.txt).
+- A prettified version of the same protocol can be seen [here](https://digitalsirkeith.github.io/coe151/mp1protocol.html).
+- Please do note of the expiration date as we have not modified it since March 2020.
--- library/peer/__init__.py
+++ library/peer/__init__.py
@@ -15,6 +15,12 @@ def prompt_for_port():
 def prompt_for_name():
     return input('Enter username: ')
 
+def prompts_for_messages(handler, tracker):
+    while True:
+        user_input = input('')
+        if handler.handle_input(user_input, tracker):
+            return
+
 def run():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as peer_socket:
         if config.USE_DEFAULT_PORT:
@@ -45,15 +51,27 @@ def run():
                 print(f'That username already exists!')
 
         tracker.handshake(user_name)
-        while True:
-            read_list, _, __ = select.select([sys.stdin, peer_socket], [], [])
+        if config.OS == 'UNIX':
+            while True:
+                read_list, _, __ = select.select([sys.stdin, peer_socket], [], [])
 
-            for readable in read_list:
-                if sys.stdin is readable:
-                    user_input = input('')
-                    if handler.handle_input(user_input, tracker):
-                        return
-                elif peer_socket is readable:
-                    message, addr = tracker.receive_message()
-                    if handler.handle_message(message, tracker, addr):
-                        return
+                for readable in read_list:
+                    if sys.stdin is readable:
+                        user_input = input('')
+                        if handler.handle_input(user_input, tracker):
+                            return
+                    elif peer_socket is readable:
+                        message, addr = tracker.receive_message()
+                        if handler.handle_message(message, tracker, addr):
+                            return
+        else:
+            from threading import Thread
+            input_thread = Thread(target=prompts_for_messages, args=(handler, tracker))
+            input_thread.start()
+            while True:
+                message, addr = tracker.receive_message()
+                if handler.handle_message(message, tracker, addr):
+                    input_thread.join()
+                    return
+                if input_thread.is_alive() is not True:
+                    return
